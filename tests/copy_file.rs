b68fdaab5e83891f00109cfd5352f7b80b95f0fa
Ondřej Maxa <ondrej.maxa11@gmail.com>
2024-06-22 15:34:22 +0200

    fix: use file in main filesystem, not temp

--- tests/copy_file.rs
+++ tests/copy_file.rs
@@ -1,18 +1,14 @@
 use std::env;
-
-use assert_fs::fixture::{FileWriteStr, PathChild};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
 };
 
 #[tokio::test]
-async fn copy_tcp() {
+async fn copy_file() {
     env::set_var("RUST_LOG", "debug");
     env::set_var("APP_ENV", "d");
     essentials::install();
-    let mock_file = assert_fs::TempDir::new().unwrap().child("mock_file");
-    mock_file.write_str("hi").unwrap();
     let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
     let addr = listener.local_addr().unwrap();
     tokio::spawn(async move {
@@ -20,12 +16,20 @@ async fn copy_tcp() {
         let mut buf = [0; 1024];
         let n = left_rx.read(&mut buf).await.unwrap();
         assert_eq!(&buf[..n], b"hello");
-        let mut mock_file = tokio::fs::File::open(mock_file.path()).await.unwrap();
+        let mut mock_file = tokio::fs::File::open("long_file.txt").await.unwrap();
         ::io::copy_file(&mut mock_file, &mut left_tx).await.unwrap();
     });
     let mut client = tokio::net::TcpStream::connect(&addr).await.unwrap();
     client.write_all(b"hello").await.unwrap();
+    let mut len = 0;
     let mut buf = [0; 1024];
-    let n = client.read(&mut buf).await.unwrap();
-    assert_eq!(&buf[..n], b"hi");
+    loop {
+        let n = client.read(&mut buf).await.unwrap();
+        if n == 0 {
+            break;
+        }
+        len += n;
+    }
+    // 20 MB file
+    assert_eq!(len, 20 * 1024 * 1024);
 }
--- tests/copy_full_example.rs
+++ tests/copy_full_example.rs
@@ -1,19 +1,15 @@
-use std::env;
-
-use assert_fs::fixture::{FileWriteStr, PathChild};
 use futures_util::future::join_all;
+use std::env;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
 };
 
 #[tokio::test]
-async fn copy_tcp() {
+async fn copy_full() {
     env::set_var("RUST_LOG", "debug");
     env::set_var("APP_ENV", "d");
     essentials::install();
-    let mock_file = assert_fs::TempDir::new().unwrap().child("mock_file");
-    mock_file.write_str("hi").unwrap();
     let mock_listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
     let mock_addr = mock_listener.local_addr().unwrap();
     tokio::spawn(async move {
@@ -21,7 +17,7 @@ async fn copy_tcp() {
         let mut buf = [0; 1024];
         let n = left_rx.read(&mut buf).await.unwrap();
         assert_eq!(&buf[..n], b"hello");
-        let mut mock_file = tokio::fs::File::open(mock_file.path()).await.unwrap();
+        let mut mock_file = tokio::fs::File::open("long_file.txt").await.unwrap();
         ::io::copy_file(&mut mock_file, &mut left_tx).await.unwrap();
     });
     let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
@@ -40,7 +36,15 @@ async fn copy_tcp() {
     });
     let mut client = tokio::net::TcpStream::connect(&addr).await.unwrap();
     client.write_all(b"hello").await.unwrap();
+    let mut len = 0;
     let mut buf = [0; 1024];
-    let n = client.read(&mut buf).await.unwrap();
-    assert_eq!(&buf[..n], b"hi");
+    loop {
+        let n = client.read(&mut buf).await.unwrap();
+        if n == 0 {
+            break;
+        }
+        len += n;
+    }
+    // 20 MB file
+    assert_eq!(len, 20 * 1024 * 1024);
 }
