cb7cee0bbe7214960948d8aa766bb8306cc60684
Ondřej Maxa <ondrej.maxa11@gmail.com>
2024-06-22 19:40:20 +0200

    fix: synchronize copy_file return type

--- src/copy/file/mod.rs
+++ src/copy/file/mod.rs
@@ -10,10 +10,9 @@ use tokio::{fs::File, io, net::tcp::OwnedWriteHalf};
 /// Copy data from a file to a write half.
 /// This function is only available on non-linux platforms and uses tokio::io::copy.
 #[cfg(not(target_os = "linux"))]
-pub async fn copy<'a>(r: &'a mut File, w: &'a mut OwnedWriteHalf) -> io::Result<()> {
+pub async fn copy<'a>(r: &'a mut File, w: &'a mut OwnedWriteHalf) -> io::Result<usize> {
     use essentials::debug;
 
     debug!("copying file to tcp stream using tokio::io::copy");
-    io::copy(r, w).await?;
-    Ok(())
+    io::copy(r, w).await.map(|x| x as usize)
 }
