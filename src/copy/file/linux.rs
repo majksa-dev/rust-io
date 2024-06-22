5120a2c98b7a8232ccec7b72e480c3c4e5892df2
Ondřej Maxa <ondrej.maxa11@gmail.com>
2024-06-22 15:25:48 +0200

    fix: swap reader and writer

--- src/copy/file/linux.rs
+++ src/copy/file/linux.rs
@@ -18,7 +18,7 @@ pub async fn copy<'a>(r: &'a mut File, w: &'a mut OwnedWriteHalf) -> io::Result<
     let mut n: usize = 0;
     loop {
         w.as_ref().writable().await?;
-        match unsafe { libc::sendfile(rfd, wfd, ptr::null_mut(), usize::MAX) } {
+        match unsafe { libc::sendfile(wfd, rfd, ptr::null_mut(), usize::MAX) } {
             x if x > 0 => n += x as usize,
             0 => {
                 break;
